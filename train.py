006de944b66d5cdda4448bcb5db9288a29a24f40
Le Quoc Loi <quocloihoak34ctn@gmail.com>
2026-07-29 16:39:18 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,27 +1,56 @@
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+
+from sklearn.metrics import classification_report
+
+
+# Đọc dữ liệu
 df = pd.read_csv("data/heart.csv")
-# x là input  
-# y là output 
+
+# Tách dữ liệu
 X = df.drop("target", axis=1)
 y = df["target"]
-'''
-print(X.head())
 
-print()
-
-print(y.head())
-'''
-
+# Chia train/test
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
-    test_size=0.2, # xử lý overfitting bằng validation 
-    #( học 20 %  xấp xỉ 1025*20% = 205 kiểm tra còn 820 để học )
-    random_state=0 #CROSS VALIDATION
+    test_size=0.2,
+    random_state=42
 )
-print(X_train.shape)
-print(X_test.shape)
 
-print(y_train.shape)
-print(y_test.shape)
+# Chuẩn hóa
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Khởi tạo mô hình
+model = LogisticRegression()
+
+# Huấn luyện
+model.fit(X_train, y_train)
+
+# Dự đoán
+y_pred = model.predict(X_test)
+
+# Đánh giá
+accuracy = accuracy_score(y_test, y_pred)
+
+print(f"Accuracy: {accuracy:.4f}")
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot()
+plt.show()
+
+print(classification_report(y_test, y_pred))
